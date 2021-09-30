919a7439ba15cac9111dcc9a3df1641e8e649687
cgonel <gonelw@gmail.com>
2021-09-30 12:13:36 -0400

    migrations

--- database/factories/SchoolNameFactory.php
+++ database/factories/SchoolNameFactory.php
@@ -7,6 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(SchoolName::class, function (Faker $faker) {
     return [
-        //
+        'id' => $this->faker->uuid(),
+        'school_name' => $this->faker->company()
     ];
 });
