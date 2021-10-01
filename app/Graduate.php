d33aa017076fce6162f6d123adc96cc5cdbb6149
Gazi Md Ashab Hossain <takveer@gmail.com>
2021-10-01 00:13:07 -0300

    Update Graduate.php

--- app/Graduate.php
+++ app/Graduate.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Graduate extends Model
 {
     protected $table = 'graduates';
+    protected $table = 'undergrad';
 
     function graduate_degrees(){
         return $this->hasMany('App\GraduateDegree', 'graduate_id', 'id');
